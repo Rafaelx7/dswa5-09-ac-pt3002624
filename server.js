01e487f7fe9dfbab851b56845741b643dc41280a
RafaelX7 <rafael.bneves7@gmail.com>
2020-10-05 17:33:22 -0300

    merge

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var app = require('./config/express')();
+const url = 'mongodb+srv://dbUser_Application:WEGLXrQbJi0i8gm2@cluster.azvt4.azure.mongodb.net/ifsp?retryWrites=true&w=majority';
+
+require('./config/database.js')(url);
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express Server escutando na porta ' + app.get('port'));
+});
